4b5dbbece8898203ec3013ff0e209cef548e6286
BrianJVarley <brianvarley9@gmail.com>
2014-11-30 23:03:27 +0000

    second commit

--- src/ie/gmit/computing/MainActivity.java
+++ src/ie/gmit/computing/MainActivity.java
@@ -4,14 +4,34 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.os.Environment;
+import android.widget.ImageView;
 
 
 public class MainActivity extends ActionBarActivity {
 
+	private static final int CAMERA_PIC_REQUEST = 1111;
+    private ImageView mImage;
+ 
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        mImage = (ImageView) findViewById(R.id.camera_image);
+        //1
+        Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, CAMERA_PIC_REQUEST);
+        
     }
 
 
@@ -21,6 +41,32 @@ public class MainActivity extends ActionBarActivity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
+    
+    
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAMERA_PIC_REQUEST) {
+            //2
+            Bitmap thumbnail = (Bitmap) data.getExtras().get("data");  
+            mImage.setImageBitmap(thumbnail);
+            //3
+            ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+            thumbnail.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+            //4
+            File file = new File(Environment.getExternalStorageDirectory()+File.separator + "image.jpg");
+            try {
+                file.createNewFile();
+                FileOutputStream fo = new FileOutputStream(file);
+                //5
+                fo.write(bytes.toByteArray());
+                fo.close();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
+    
+    
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
