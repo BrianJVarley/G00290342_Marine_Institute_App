7a4ff5a21a6fc1f2d51f66cb3659042368424434
BrianJVarley <brianvarley9@gmail.com>
2014-12-20 22:34:58 +0000

    Saturday Commit 20th December

--- src/ie/gmit/computing/MainActivity.java
+++ src/ie/gmit/computing/MainActivity.java
@@ -1,82 +1,54 @@
 package ie.gmit.computing;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import android.app.Activity;
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.os.Bundle;
-import android.os.Environment;
-import android.widget.ImageView;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
-
-	private static final int CAMERA_PIC_REQUEST = 1111;
-    private ImageView mImage;
- 
 	
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        
-        mImage = (ImageView) findViewById(R.id.camera_image);
-        //1
-        Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivityForResult(intent, CAMERA_PIC_REQUEST);
-        
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+	
+	
+	public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-    
-    
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == CAMERA_PIC_REQUEST) {
-            //2
-            Bitmap thumbnail = (Bitmap) data.getExtras().get("data");  
-            mImage.setImageBitmap(thumbnail);
-            //3
-            ByteArrayOutputStream bytes = new ByteArrayOutputStream();
-            thumbnail.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
-            //4
-            File file = new File(Environment.getExternalStorageDirectory()+File.separator + "image.jpg");
-            try {
-                file.createNewFile();
-                FileOutputStream fo = new FileOutputStream(file);
-                //5
-                fo.write(bytes.toByteArray());
-                fo.close();
-            } catch (IOException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-        }
-    }
-    
-    
+	
+	
+	
+	public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+        case R.id.item1:
+          Toast.makeText(this, "Option1", Toast.LENGTH_SHORT).show();
+          return true;
+        case R.id.item2:
+          Toast.makeText(this, "Option2", Toast.LENGTH_SHORT).show();
+          return true;
+        case R.id.item3:
+          Toast.makeText(this, "Option3", Toast.LENGTH_SHORT).show();
+          return true;           
+        case R.id.item4:
+          Toast.makeText(this, "Option4", Toast.LENGTH_SHORT).show();
+          return true;
+        case R.id.item5:
+          Toast.makeText(this, "Option5", Toast.LENGTH_SHORT).show();
+          return true;
+        case R.id.item6:
+          Toast.makeText(this, "Option6", Toast.LENGTH_SHORT).show();
+          return true;           
+        default:
+          return super.onOptionsItemSelected(item);
+        } 
+    } 
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+	
 }
