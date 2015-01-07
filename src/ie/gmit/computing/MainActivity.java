5afa68f8eabef25e67e86065585029a03732722e
BrianJVarley <brianvarley9@gmail.com>
2015-01-07 22:11:51 +0000

    Debri match functionality added

--- src/ie/gmit/computing/MainActivity.java
+++ src/ie/gmit/computing/MainActivity.java
@@ -2,43 +2,28 @@ package ie.gmit.computing;
  
 //import com.example.cameratest.R;
  
-import ie.gmit.computing.model.Node;
-
 import java.io.File;
-import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
- 
-import android.app.AlertDialog;
-import android.content.Context;
-import android.content.DialogInterface;
+
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
-import android.location.Criteria;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
-import android.media.ExifInterface;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
-import android.provider.MediaStore.Files;
 import android.support.v7.app.ActionBarActivity;
-import android.text.Editable;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
  
  
@@ -56,7 +41,6 @@ public class MainActivity extends ActionBarActivity implements OnClickListener{
 	//can be used to cross reference csv record with image number.
 	private int i = 0;
 	
-	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
@@ -66,14 +50,15 @@ public class MainActivity extends ActionBarActivity implements OnClickListener{
 		photoImage.setImageDrawable(null);
 	
 		
-		Button mClickButton1 = (Button)findViewById(R.id.cameraBtn);
-		mClickButton1.setOnClickListener(this);
-		Button mClickButton2 = (Button)findViewById(R.id.searchBtn);
-		mClickButton2.setOnClickListener(this);
-		Button mClickButton3 = (Button)findViewById(R.id.resBtn);
-		mClickButton3.setOnClickListener(this);
+		Button camClickButton = (Button)findViewById(R.id.cameraBtn);
+		camClickButton.setOnClickListener(this);
+		Button searchClickButton = (Button)findViewById(R.id.searchBtn);
+		searchClickButton.setOnClickListener(this);
+		Button restClickButton = (Button)findViewById(R.id.resBtn);
+		restClickButton.setOnClickListener(this);
 		
-
+		 
+		
 	    
 	}
 	
@@ -89,19 +74,8 @@ public class MainActivity extends ActionBarActivity implements OnClickListener{
 	            // start camera intent
 	        	 Toast.makeText(this, "camera clicked", Toast.LENGTH_SHORT).show();
 	        	 Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-	 	         try {
-					File file = getOutputPhotoFile();
- 
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-	 	         try {
-					fileUri = Uri.fromFile(getOutputPhotoFile());
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
+	 	         File file = getOutputPhotoFile();
+	 	         fileUri = Uri.fromFile(getOutputPhotoFile());
 	 	         i.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
 	 	         startActivityForResult(i, CAPTURE_IMAGE_ACTIVITY_REQ );
 	            break;
@@ -151,7 +125,7 @@ public class MainActivity extends ActionBarActivity implements OnClickListener{
 	
 	
 	
-	private File getOutputPhotoFile() throws IOException {
+	private File getOutputPhotoFile() {
 		  File directory = new File(Environment.getExternalStoragePublicDirectory(
 		                Environment.DIRECTORY_PICTURES), getPackageName());
 		  if (!directory.exists()) {
@@ -160,27 +134,10 @@ public class MainActivity extends ActionBarActivity implements OnClickListener{
 		      return null;
 		    }
 		  }
-		  
-		  
 		  String timeStamp = new SimpleDateFormat("yyyMMdd_HHmmss", Locale.ENGLISH).format(new Date());
-		 
-		  
-		  
-		  File[] files = directory.listFiles();
-		  i++;
-		  File origFile =  new File(directory.getPath(), "IMG_" + "# " +i + "_" + timeStamp + ".jpg");
-		  
-		  
-		  
-		  if(files.length!=0) {
-		      File newestFile = files[files.length-1];
-		      origFile =  new File(directory.getPath() + File.separator + newestFile.getName());
-		  }
-		   
-	
-	  return origFile; 
-		  
-	}
+		  return new File(directory.getPath() + File.separator + "IMG_"  
+		                    + timeStamp + ".jpg");
+		}
 	  
 	  protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		  if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQ) {
@@ -195,12 +152,7 @@ public class MainActivity extends ActionBarActivity implements OnClickListener{
 		        Toast.makeText(this, "Image saved successfully in: " + data.getData(), 
 		                       Toast.LENGTH_LONG).show();
 		      }
-		      try {
-				showPhoto(photoUri);
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+		      showPhoto(photoUri);
 		    } else if (resultCode == RESULT_CANCELED) {
 		      Toast.makeText(this, "Cancelled", Toast.LENGTH_SHORT).show();
 		    } else {
@@ -214,10 +166,10 @@ public class MainActivity extends ActionBarActivity implements OnClickListener{
 	 
 	
 	  
-	  private void showPhoto(Uri photoUri) throws IOException {
+	  private void showPhoto(Uri photoUri) {
 		  String filePath = photoUri.getEncodedPath();
 		  File imageFile = new File(filePath);
-		  
+		  //File imageFile = new File(photoUri.getPath());
 		  if (imageFile.exists()){
 			 Drawable oldDrawable = photoImage.getDrawable(); 
 			 if (oldDrawable != null) { 
@@ -227,9 +179,9 @@ public class MainActivity extends ActionBarActivity implements OnClickListener{
 		     BitmapDrawable drawable = new BitmapDrawable(this.getResources(), bitmap);
 		     photoImage.setScaleType(ImageView.ScaleType.FIT_CENTER);
 		     photoImage.setImageBitmap(Bitmap.createScaledBitmap(bitmap, 130, 110, false));
-		  }
-		  
+		  }       
 		}
+
 	  
 	
 }
